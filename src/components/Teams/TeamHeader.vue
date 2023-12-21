70a262f96dda6c30ef2648d408874eb6336dff7d
Xavier0x35e81 <xavier.0x35e81@gmail.com>
2023-12-21 15:54:52 +0800

    Extracted & combined `AddMember` button & modal with teleport

--- src/components/Teams/TeamHeader.vue
+++ src/components/Teams/TeamHeader.vue
@@ -1,21 +1,13 @@
 <script setup>
 import { useTeamStore } from '@/stores/TeamStore.js'
+import AddMemberModal from '@/components/Teams/AddMemberModal.vue'
 
 let team = useTeamStore()
 </script>
 
 <template>
   <header class="flex justify-between">
-    <div>
-      <button
-        class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
-        :disabled="!team.spotsRemaining"
-        @click="$emit('add')"
-      >
-        Add Member ({{ team.spotsRemaining }} Spots Left)
-      </button>
-    </div>
-
+    <AddMemberModal />
     <div>
       <h3 class="inline-flex items-center text-3xl relative">
         <img src="" alt="" class="mr-2" />
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -2,9 +2,7 @@
 import TeamHeader from '@/components/Teams/TeamHeader.vue'
 import TeamFooter from '@/components/Teams/TeamFooter.vue'
 import TeamMemberList from '@/components/Teams/TeamMemberList.vue'
-import Modal from '@/components/Modal.vue'
 import { useTeamStore } from '@/stores/TeamStore.js'
-import { ref } from 'vue'
 
 let team = useTeamStore()
 team.fill()
@@ -12,25 +10,14 @@ team.fill()
 setTimeout(() => {
   team.grow(25)
 }, 1000)
-
-let showModal = ref(false)
 </script>
 
 <template>
-  <Modal :show="showModal" @close="showModal = false">
-    <template #default>
-      <p>Need to add a new member to your team?</p>
-      <form class="mt-6">
-        <div class="flex gap-2">
-          <input type="email" placeholder="Email Address" class="flex-1" />
-          <button>Add</button>
-        </div>
-      </form>
-    </template>
-  </Modal>
-  <TeamHeader @add="showModal = true" />
+  <TeamHeader />
+
   <div class="place-self-center flex flex-col gap-y-3" style="width: 725px">
     <TeamMemberList />
   </div>
+
   <TeamFooter />
 </template>
