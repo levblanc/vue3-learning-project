c65c2976674b4bffdc8131b31d3659ce16b749cb
levblanc <levblanc@gmail.com>
2023-12-21 15:16:56 +0800

    Added `TeamMemberList` component

---
+++ src/components/Teams/TeamMemberList.vue
@@ -0,0 +1,28 @@
+<script setup>
+import TeamMember from '@/components/Teams/TeamMember.vue'
+import { useTeamStore } from '@/stores/TeamStore.js'
+
+let team = useTeamStore()
+</script>
+
+<template>
+  <table class="table-fixed border-spacing-2 border-separate">
+    <thead>
+      <th class="text-left px-6 py-2">Name</th>
+      <th class="text-left px-6 py-2">Email</th>
+      <th class="text-left px-6 py-2">Status</th>
+    </thead>
+    <tbody>
+      <TeamMember
+        v-for="member in team.members"
+        :key="member.name"
+        :name="member.name"
+        :email="member.email"
+        :status="member.status"
+      />
+    </tbody>
+  </table>
+  <p class="text-right text-gray-600 italic" v-show="!team.spotsRemaining">
+    There are no remaining team spots. Upgrade to add more.
+  </p>
+</template>
