5ec2a64cdd24d06bd44b55197a7df2b2ef9b3573
levblanc <levblanc@gmail.com>
2023-12-21 15:16:43 +0800

    Added `TeamFooter` component

---
+++ src/components/Teams/TeamFooter.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { useTeamStore } from '@/stores/TeamStore.js'
+
+let team = useTeamStore()
+</script>
+
+<template>
+  <footer class="mt-12 bg-gray-100 py-4 text-center">
+    <h5 class="font-semibold text-lg">{{ team.name }} - {{ team.spots }} Member Team</h5>
+  </footer>
+</template>
