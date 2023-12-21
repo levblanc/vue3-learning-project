38cd2863a6b0479c5b86e2346b935cd70a56867f
levblanc <levblanc@gmail.com>
2023-12-21 15:16:33 +0800

    Added `TeamHeader` component

---
+++ src/components/Teams/TeamHeader.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { useTeamStore } from '@/stores/TeamStore.js'
+
+let team = useTeamStore()
+</script>
+
+<template>
+  <header class="flex justify-between">
+    <div>
+      <button
+        class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
+        :disabled="!team.spotsRemaining"
+        @click="$emit('add')"
+      >
+        Add Member ({{ team.spotsRemaining }} Spots Left)
+      </button>
+    </div>
+
+    <div>
+      <h3 class="inline-flex items-center text-3xl relative">
+        <img src="" alt="" class="mr-2" />
+        {{ team.name }} Team
+        <div
+          class="bg-green-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2"
+        >
+          {{ team.spots }}
+        </div>
+      </h3>
+    </div>
+  </header>
+</template>
